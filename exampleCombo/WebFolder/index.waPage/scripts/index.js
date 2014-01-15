9f96019614b51acd4e3983837d52518fe5c121c4
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-01-15 20:14:54 +0200

    Initial commit

---
+++ exampleCombo/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,19 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button2 = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button2.click = function button2_click (event)// @startlock
+	{// @endlock
+		sources.person.gender.set(sources.gender);
+		sources.person.save();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button2", "click", button2.click, "WAF");
+// @endregion
+};// @endlock
